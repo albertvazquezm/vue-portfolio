fb976086fa02596b76e942f017d179112c734023
Albert Vazquez <Albert@iMac-de-Albert.local>
2016-03-09 16:10:04 +0100

    Store data in plain JSON instead of JavaScript Objects

--- src/data/common.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    email : "hola@albertvazquez.es",
-    gitHub : "https://github.com/albertvazquezm",
-    linkedIn : "http://linkedin.com/",
-    codepen : "http://codepen.io/albertvazquezm/"
-}
