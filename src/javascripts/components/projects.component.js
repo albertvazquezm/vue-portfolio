f0c5c6997eabece37aff7edb5306d702a4746991
Albert Vazquez <Albert@iMac-de-Albert.local>
2016-03-09 16:10:36 +0100

    Create projects component basic layout

--- src/javascripts/components/projects.component.js
+++ src/javascripts/components/projects.component.js
@@ -1,20 +1,33 @@
 'use strict';
 
-import projectsData from '../../data/projects.js';
+import data from '../../data/projects.json';
 
 module.exports =  {
     template : `
-        <h2>Projects</h2>
-        <ul v-for="project in projects">
-            <li>{{project.name}}</li>
-        </ul>
+        <div class="projects">
+            <h2 class="subtitle is-4">Projects</h2>
+            <article v-for="project in projects">
+                <h3>{{project.role}}</h3><span class="top-bullet">{{project.time}}</span>
+                <span class="subtitle1">{{project.name}}</span>
+                for
+                <span class="subtitle2">{{project.client}}</span>
+                <p class="description">{{project.description}}</p>
+                <span v-for="skill in project.skills" class="tag is-success">
+                    {{skill}}
+                </span>
+                <span v-for="library in project.libraries" class="tag is-warning">
+                    {{library}}
+                </span>
+                <a href="{{project.url}}">{{project.url}}</a>
+            </article>
+        </div>
     `,
     props : [
         
     ],
     data : () => {
         return {
-            projects : projectsData
+            projects : data
         }
     }
 };
